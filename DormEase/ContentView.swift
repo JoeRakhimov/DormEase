d549c93b5dc516b8820cc8691dc2dc6ceb9e7e5a
Arsenii Dudyk <arsengggj@gmail.com>
2023-10-19 17:47:04 +0200

    Update .gitignore

--- DormEase/ContentView.swift
+++ DormEase/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, Joe!")
+            Text("Hello, Arsenii!")
         }
         .padding()
     }
