7f0b191d80e551028084a5b117627b206777cdfc
Joe <joerakhimov@gmail.com>
2023-10-19 14:57:37 +0200

    Initial Commit

---
+++ DormEase/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DormEase
+//
+//  Created by Gayrat Rakhimov on 19/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DormEase/DormEaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DormEaseApp.swift
+//  DormEase
+//
+//  Created by Gayrat Rakhimov on 19/10/23.
+//
+
+import SwiftUI
+
+@main
+struct DormEaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
