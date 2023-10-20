41c522ddc5f9038ae62a1ebf217665e049f6e2dd
Arsenii Dudyk <arsengggj@gmail.com>
2023-10-20 14:40:10 +0200

    Privit

--- DormEase/ContentView.swift
+++ DormEase/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Salom, Arsenii!")
+            Text("Privit, Arsenii!")
         }
         .padding()
     }
