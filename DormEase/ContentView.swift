4289936633c5bec328cbda1ee1538366e200b7e8
Arsenii Dudyk <arsengggj@gmail.com>
2023-10-20 14:43:50 +0200

    i changed ciao to chsc

--- DormEase/ContentView.swift
+++ DormEase/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Ciao, Arsenii!")
+            Text("chesc, Arsenii!")
         }
         .padding()
     }
