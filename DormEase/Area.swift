91cc74ce6a1f62a046d528baae93d45c361b3af9
bilegentile <76962962+BileGentile@users.noreply.github.com>
2023-10-20 17:52:36 +0200

    Created Area and AreaViewModel files - Popluated with some placeholders AreaViewModel

---
+++ DormEase/Area.swift
@@ -0,0 +1,8 @@
+//
+//  Area.swift
+//  DormEase
+//
+//  Created by Beniamino Gentile on 20/10/23.
+//
+
+import Foundation
---
+++ DormEase/AreaViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AreaViewModel.swift
+//  DormEase
+//
+//  Created by Beniamino Gentile on 20/10/23.
+//
+
+import Foundation
