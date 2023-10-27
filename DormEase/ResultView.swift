f847f361f45aa609b77352f2f859cba0c32c0110
Arsenii Dudyk <arsengggj@gmail.com>
2023-10-27 14:21:02 +0200

    i added result view (test)

---
+++ DormEase/ResultView.swift
@@ -0,0 +1,18 @@
+//
+//  ResultView.swift
+//  NaplesNest
+//
+//  Created by Arsenii Dudyk on 26/10/23.
+//
+
+import SwiftUI
+
+struct ResultView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ResultView()
+}
