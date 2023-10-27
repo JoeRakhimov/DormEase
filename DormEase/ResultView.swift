c6b3ca51c4332e2146cdde3a165d5cc715dfd620
Arsenii Dudyk <arsengggj@gmail.com>
2023-10-27 15:07:50 +0200

    I aded a shape of resultview(for 2nd time 😐) and used san giovanni barra as a example

--- DormEase/ResultView.swift
+++ DormEase/ResultView.swift
@@ -15,15 +15,23 @@ struct ResultView: View {
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                     .frame(width: 2000, height: 250)
-                    
-             
+                    .offset(x: 0, y: -115)
+                
+                Text("San Giovanni-Barra")
+                    .foregroundColor(.white)
+                    .font(.title)
+                    .padding()
+                    .background(Color.orange.opacity(0.7))
+                    .cornerRadius(15)
+                    .offset(x: 0, y: -120)
             }
             
-            Text("Additional Information")
+            Text("University of Naples Federico II - Polytechnic and Basic Sciences School - Naples East Complex\nAddress: Corso Nicolangelo Protopisani, 70, 80146 Naples ΝΑ\n\nEssential Services:\n- Municipality of Naples (Town Hall)\n- UniCredit\n- Post Office - Poste Italiane\n- Decò Maxistore\n\nTransport:\n- Line 2 metro stop: San Giovanni - Barra\n- Circumvesuviana stop: S. Giovanni a Teduccio\n\nAttractions:\n- Church Crowned Mother of Consolation\n- Madonna of the Christopher Columbus arch")
                 .foregroundColor(.black)
                 .font(.body)
+                .multilineTextAlignment(.leading)
+                .offset(x: 0, y: -70)
         }
-        .padding()
     }
 }
 
