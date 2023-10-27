aa03019e7d387fae3dfee16ae5022d25d29b368d
bilegentile <76962962+BileGentile@users.noreply.github.com>
2023-10-27 15:28:10 +0200

    Added Area Model and Area Model View

--- DormEase/AreaViewModel.swift
+++ DormEase/AreaViewModel.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+
+class AreaViewModel {
+    
+    var areas = [
+        //Municipalità 1
+        
+        Area(areaName: "Posillipo"),
+        //Municipalità 2
+        
+        Area(areaName: "San Giuseppe (Centro Storico"),
+        Area(areaName: "Avvocata"),
+        //Municipalità 3
+        
+        Area(areaName: "Stella"),
+        //Municipalità 4
+        
+        //Municipalità 5
+        
+        Area(areaName: "Vomero"),
+        //Municipalità 6
+        
+        Area(areaName: "San Giovanni a Teduccio"),
+        //Municipalità 7
+        
+        //Municipalità 8
+        
+        //Muncipalità 9
+        
+        //Municipalità 10
+        
+        Area(areaName: "Fuorigrotta")
+        
+    ]
+}
